e7188a43763d665cbbdef9f5f48703b58528f5f9
niqdevgit <107034072+niqdevgit@users.noreply.github.com>
2024-10-30 20:33:17 +0200

    Blog added

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,5 +1,9 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
+
+//TODO add fixed size to cards, desc and skills can wrap. Tell more about each project.
+//add images. Add intrest in info section eg. DevOps CI/CD git Docker...
+//better styling?
 
 const projectsData = [
   {
@@ -53,7 +57,14 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Projects</h1>
+      <h1>Hello! Nice to see you here 👋</h1>
+      <span>Find my education and work history on <a href='https://www.linkedin.com/in/niklassuvitie/'>LinkedIn</a></span>
+      <br></br>
+      <span><a href='https://niqdevgit.github.io/blog'>View my blog</a></span>
+      <br></br>
+      <span>Here are my personal or school projects. <br></br>
+       My professional works are not included here. <br></br>
+       Click any title to view project site.</span>
       <div className="filter-bar">
         <input
           type="text"
@@ -67,7 +78,7 @@ const App = () => {
         {filteredProjects.length > 0 ? (
           filteredProjects.map((project) => (
             <div key={project.id} className="project-card">
-              <h2><a href={project.link}>{project.title}</a></h2>
+              <h3><a href={project.link}>{project.title}</a></h3>
               <p>{project.description}</p>
               <div className="skills">
                 {project.skills.map((skill, index) => (
